3a77904455e59986c1f8162148c98e1c32e50ea2
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2019-05-11 21:50:06 +0200

    Provide a decode interface

--- src/Decode.elm
+++ src/Decode.elm
@@ -1,4 +1,7 @@
-module Csv.Decode exposing (Decoder)
+module Csv.Decode exposing
+    ( Decoder, Csv, Error
+    , decode
+    )
 
 {-| Turn CSV Values into Elm values. Inspired by [`elm/json`][elm-json], so make sure to check out this [intro to
 JSON decoders][guide] to get a feel for how this library works!
@@ -18,12 +21,87 @@ This library gets you the rest of the way, to a list of your own types.
 [periodic]: https://package.elm-lang.org/packages/periodic/elm-csv/latest/
 [lovasoa]: https://package.elm-lang.org/packages/lovasoa/elm-csv/latest/
 
+
+# Types
+
+@docs Decoder, Csv, Error
+
+
 # Primitives
 
-@docs Decoder
+
+# Run Decoders
+
+@docs decode
 
 -}
 
 
+{-| The raw CSV data structure.
+-}
+type alias Csv =
+    { header : List String
+    , records : List (List String)
+    }
+
+
+{-| A value that knows how to decode CSV values.
+-}
 type Decoder a
-    = Decoder
+    = Decoder (List String -> Result Error a)
+
+
+{-| A structured error describing exactly how the decoder failed. You can use
+this to create more elaborate visualizations of a decoder problem. For example,
+you could show the entire CSV record and show the part causing the failure in
+red.
+-}
+type Error
+    = UnwrapErrorProblem
+    | GeneralError
+    | MultipleErrors (List ( Int, Error ))
+
+
+{-| Decode the given `Csv` into a custom value by running `Decoder` on it.
+This will fail if any of the records can not be decoded by the `Decoder` for some reason.
+-}
+decode : Decoder a -> Csv -> Result Error (List a)
+decode (Decoder mapper) { records } =
+    records
+        |> List.map mapper
+        |> gather
+
+
+gather : List (Result Error a) -> Result Error (List a)
+gather results =
+    let
+        split ( index, result ) ( errors, vs ) =
+            case result of
+                Ok v ->
+                    ( errors, v :: vs )
+
+                Err error ->
+                    ( ( index, error ) :: errors, vs )
+
+        ( indexedErrors, values ) =
+            results
+                |> List.indexedMap Tuple.pair
+                |> List.foldr split ( [], [] )
+    in
+    if List.isEmpty indexedErrors then
+        values
+            |> List.reverse
+            |> Ok
+
+    else
+        indexedErrors
+            |> List.reverse
+            |> MultipleErrors
+            |> Err
+
+
+isError : Result e t -> Bool
+isError result =
+    result
+        |> Result.map (\_ -> True)
+        |> Result.withDefault False
